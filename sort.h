93e32e55b0c75a52f682fe0b26e52f2ac13c4e25
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-02-17 00:13:14 -0800

    Add helper functions

--- sort.h
+++ sort.h
@@ -34,6 +34,8 @@ typedef struct listint_s
 } listint_t;
 
 void swap_integers(int *num1, int *num2);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
 
