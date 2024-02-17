ef6163c6479603f6030279d97e515f935ccde799
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-02-17 00:21:43 -0800

    Insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+
+/**
+ * swap_nodes - Swap two nodes
+ * @h: A pointer to the head
+ * @node1: Pointer to the first node
+ * @node2: The second node
+ */
+
+void swap_nodes(listint_t **h, listint_t **node1, listint_t *node2)
+{
+	(*node1)->next = node2->next;
+	if (node2->next != NULL)
+		node2->next->prev = *node1;
+	node2->prev = (*node1)->prev;
+	node2->next = *node1;
+	if ((*node1)->prev != NULL)
+		(*node1)->prev->next = node2;
+	else
+		*h = node2;
+	(*node1)->prev = node2;
+	*node1 = node2->prev;
+}
+
+/**
+ * insertion_sort_list - sorts a doubly linked list
+ * @lst: A pointer to the head of the doubly linked list
+ */
+
+/* Time Complexity: O(n^2)*/
+void insertion_sort_list(listint_t **lst)
+{
+	listint_t *itr, *insert, *tmp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	for (itr = (*lst)->next; itr != NULL; itr = tmp)
+	{
+		tmp = itr->next;
+		insert = itr->prev;
+		while (insert != NULL && itr->n < insert->n)
+		{
+			swap_nodes(lst, &insert, itr);
+			print_list((const listint_t *)*lst);
+		}
+	}
+}
