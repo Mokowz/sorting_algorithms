3214442524175e6b45562c56b0affdca873392de
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-02-17 00:26:25 -0800

    Make changes

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -24,25 +24,25 @@ void swap_nodes(listint_t **h, listint_t **node1, listint_t *node2)
 
 /**
  * insertion_sort - sorts a doubly linked list
- * @lst: A pointer to the head of the doubly linked list
+ * @list: A pointer to the head of the doubly linked list
  */
 
 /* Time Complexity: O(n^2)*/
-void insertion_sort(listint_t **lst)
+void insertion_sort_list(listint_t **list)
 {
 	listint_t *itr, *insert, *tmp;
 
-	if (lst == NULL || *lst == NULL || (*lst)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	for (itr = (*lst)->next; itr != NULL; itr = tmp)
+	for (itr = (*list)->next; itr != NULL; itr = tmp)
 	{
 		tmp = itr->next;
 		insert = itr->prev;
 		while (insert != NULL && itr->n < insert->n)
 		{
-			swap_nodes(lst, &insert, itr);
-			print_list((const listint_t *)*lst);
+			swap_nodes(list, &insert, itr);
+			print_list((const listint_t *)*list);
 		}
 	}
 }
