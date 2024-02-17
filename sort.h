39f905e37439892471df4730af2378f1f691ba39
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-02-17 00:32:55 -0800

    Selection sort

--- sort.h
+++ sort.h
@@ -43,4 +43,6 @@ void bubble_sort(int *array, size_t size);
 
 void swap_nodes(listint_t **h, listint_t **node1, listint_t *node2);
 void insertion_sort_list(listint_t **list);
+
+void selection_sort(int *array, size_t size);
 #endif
